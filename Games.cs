71de468e8dd42d00ad5b95d8e7edb9cdff44a921
Kaelan Willauer <49886320+KaelanWillauer@users.noreply.github.com>
2020-11-28 02:05:02 -0800

    Update Games.cs

--- Games.cs
+++ Games.cs
@@ -1,89 +1,178 @@
-﻿using System;
+using System.Collections.Generic;
 using Oxide.Core.Libraries.Covalence;
+using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info("Games", "Kaelan Willauer", 0.1)]
-    [Description("A bunch of mini-games")]
-    
-    class Games : RustPlugin
+    [Info("Testing stuff", "Willauer", "0.1")]
+    public class Games : RustPlugin
     {
+        string[] gameModes = { " ", "Free For All" };
+        List<BasePlayer> playerList = new List<BasePlayer>();
+        Vector3 spawn = new Vector3(0, 100, 0);
+        bool isGame = false;
+        bool isStarted = false;
+        int ID = 0;
         int counter = 15;
-        string[] gameModes = { " FFA" };
-        int IDgamemode = 0;
 
-        private void initilizeGame(BasePlayer player, int IDgamemode)
+        private void start(int ID)
         {
-            rust.RunServerCommand("oxide.group add" + gameModes[IDgamemode]);
-            PrintToChat(player.displayName + " has started " + gameModes[IDgamemode]);
-            PrintToChat("Enter 'accept' to join!");
-            countDown(IDgamemode, counter);
+            isStarted = true;
+            PrintToChat(gameModes[ID] + " STARTING!");
+
+            switch (ID)
+            {
+                case 1:
+                    foreach (BasePlayer player in playerList)
+                    {
+                        teleport(player);
+                        giveItems(player);
+                    }
+
+                    break;
+
+            }
         }
 
-        void countDown(int IDgamemode, int counter)
+        private void initilizeGame(BasePlayer player, int ID)
+        {
+
+            isGame = true;
+            playerList.Add(player);
+            PrintToChat(player.displayName + " has started " + gameModes[ID]);
+            PrintToChat("Enter 'a' to join!");
+            countDown(ID, counter);
+            timer.Once(counter + 5, () =>
+            {
+                start(ID);
+            });
+
+
+        }
+
+        private void countDown(int ID, int counter)
         {
             timer.Repeat(counter / 3f, 3, () =>
             {
-                PrintToChat(gameModes[IDgamemode] + " starting in " + counter);
+                PrintToChat(gameModes[ID] + " starting in " + counter);
                 counter -= 5;
             });
         }
 
-        [ChatCommand("accept")] //[chatcommand("accept"), Permission(
-        private void acceptGame(BasePlayer player)
+        private void teleport(BasePlayer player)
         {
-            if (player.IPlayer.BelongsToGroup("default"))
+            Vector3 shift = spawn + new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
+            player.MovePosition(shift);
+        }
+
+        private void giveItems(BasePlayer player)
+        {
+            player.SetHealth(100);
+            switch (ID)
             {
-                rust.RunServerCommand("oxide.usergroup remove " + player.displayName + " default");
-                rust.RunServerCommand("oxide.usergroup add " + player.displayName + gameModes[IDgamemode]);
-                PrintToChat(player.displayName + " joined " + gameModes[IDgamemode]);
-                startGame(player, IDgamemode);
+                case 1:
+                    rust.RunServerCommand("inventory.giveto " + player.displayName + " lr 1");
+                    rust.RunServerCommand("inventory.giveto " + player.displayName + " ammo.rifle 200");
+                    rust.RunServerCommand("inventory.giveto " + player.displayName + " pumpkin 20");
+                    break;
             }
+
+        }
+
+        void OnPlayerRespawned(BasePlayer player)
+        {
+            switch (ID)
+            {
+                case 1:
+                    giveItems(player);
+                    teleport(player);
+                    break;
+            }
+
+        }
+        private object OnEntityTakeDamage(BaseCombatEntity entity, HitInfo hitInfo)
+        {
+            switch (ID)
+            {
+                case 1:
+                    Rust.DamageType damageType = hitInfo.damageTypes.GetMajorityDamageType();
+                    if (damageType == Rust.DamageType.Fall)
+                    {
+                        hitInfo.damageTypes.Set(damageType, 0);
+                    }
+                    break;
+            }
+            return null;
+
         }
 
         [ChatCommand("FFA")]
-        private void attemptFFA(BasePlayer player) 
-        { 
-            if (player.IPlayer.BelongsToGroup("default"))
+        private void FFA(BasePlayer player)
+        {
+            if (!isGame)
             {
-                initilizeGame(player, 0);
+                ID = 1;
+                initilizeGame(player, ID);
             }
         }
 
-        private void startGame(BasePlayer player, int IDgamemode)
+        [ChatCommand("a")]
+        private void accept(BasePlayer player)
         {
-            timer.Once(counter+5, () => // counter+5
+            if (isStarted)
             {
-                Random rnd = new System.Random();
-                int x = rnd.Next(-500, 500);
-                int z = rnd.Next(-500, 500);
-
-                switch (IDgamemode)
-                {
-                    case 0:
-                        player.Heal(100f);
-                        rust.RunServerCommand("teleport.topos " + player.displayName + " " + x.ToString() + " 80 " + z.ToString());
-                        rust.RunServerCommand("inventory.giveto " + player.displayName + " lr 1");
-                        rust.RunServerCommand("inventory.giveto " + player.displayName + " pumpkin 100");
-                        break;
-                    default:
-                        PrintToChat("IDK HOW I GOT HERE??");
-                        break;
-                }
-            });
-        }
-       
-        [ChatCommand("reset")]
-        private void reset(BasePlayer player)
-        {
-            rust.RunServerCommand("oxide.usergroup add " + player.displayName + " default");
+                PrintToChat(player, "Error: Game already started!");
+            } else if (isGame && !playerList.Contains(player))
+            {
+                playerList.Add(player);
+                PrintToChat(player.displayName + " joined " + gameModes[ID]);
+            }
         }
 
-        void Loaded()
+        [ChatCommand("setspawn")]
+        private void setSpawn(BasePlayer player)
         {
-            PrintToChat("Games Injected!");
+
+            spawn += new Vector3(player.transform.position.x, 0, player.transform.position.z);
         }
+
+
+
+        [ChatCommand("r")]
+        private void reset()
+        {
+            ID = 0;
+            isGame = false;
+
+            isStarted = false;
+
+            foreach (BasePlayer player in playerList)
+            {
+                player.DieInstantly();
+
+            }
+
+            var droppedItems = UnityEngine.GameObject.FindObjectsOfType<DroppedItem>();
+            var playerCorpses = UnityEngine.GameObject.FindObjectsOfType<PlayerCorpse>();
+            
+
+            foreach (var playerCorpse in playerCorpses)
+            {
+                playerCorpse.Kill();
+            }
+            var deadBags = UnityEngine.GameObject.FindObjectsOfType<DroppedItemContainer>();
+            foreach (var droppedItem in droppedItems)
+            {
+                droppedItem.Kill();
+            }
+            foreach (var deadBag in deadBags)
+            {
+                deadBag.Kill();
+            }
+
+            playerList = new List<BasePlayer>();
+        }
+
 
     }
-
 }
